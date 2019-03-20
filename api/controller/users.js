81590f49444bb058a51a9ab3fe17d690ef07ca98
DanFein <danfeinstat@gmail.com>
2019-03-20 15:48:05 -0700

    added controller and routes for user auth with jwt and bcrypt

---
+++ api/controller/users.js
@@ -0,0 +1,56 @@
+const userModel = require(`../models/suers`);
+const bcrypt = require(`bcrypt`);
+const jwt = require(`jsonwebtoken`);
+
+module.exports = {
+  create: (req, res, next) => {
+    userModel.create(
+      {
+        name: req.body.name,
+        email: req.body,
+        email,
+        password: req.body.password,
+      },
+      (err, result) => {
+        if (err) {
+          next(err);
+        } else {
+          res.json({
+            status: `success`,
+            message: `User added successfully`,
+            data: null,
+          });
+        }
+      }
+    );
+  },
+
+  authenticate: (req,
+  res,
+  next => {
+    userModel.findONe({ email: req.body.email }, (err, userInfo) => {
+      if (err) {
+        next(err);
+      } else {
+        if (bcrypt.compareSync(req.body.password, userInfo.password)) {
+          const token = jwt.sign(
+            { id: userInfo._id },
+            req.app.get(`secretKey`),
+            { expiresIn: `1h` }
+          );
+          res.json({
+            status: `success`,
+            message: `user found`,
+            data: { user: userInfo, token: token },
+          });
+        } else {
+          res.json({
+            status: `error`,
+            message: `Invalid email/password`,
+            data: null,
+          });
+        }
+      }
+    });
+  }),
+};
