f6931bb9630c9308a65e6a8817a2fdd4060e2b19
DanFein <danfeinstat@gmail.com>
2019-03-24 14:17:16 -0700

    updated server

--- server.js
+++ server.js
@@ -11,7 +11,16 @@ const PORT = process.env.PORT || 3002;
 
 app.use((req, res, next) => {
   res.setHeader(`Access-Control-Allow-Origin`, `*`);
-  // res.setHeader(`Access-Control-Allow-Headers`);
+
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, OPTIONS, PUT, PATCH, DELETE"
+  );
+
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Content-type,application/json,x-access-token"
+  );
   next();
 });
 
